fd111e978ebc4d12bab0ebd03000ddb0d2144fa7
sirjohnpenguin <sirjohndigweed@gmail.com>
2017-02-19 20:00:54 -0300

    phpqrcode library. phpqrcode-git-863ffffac4c9d22e522464e325cbcbadfbb26470

---
+++ files/usr/share/grase/www/radmin/includes/phpqrcode/config.php
@@ -0,0 +1,7 @@
+<?php
+define('EXAMPLE_TMP_SERVERPATH', '/usr/share/grase/qrimages/');
+//define('EXAMPLE_TMP_URLRELPATH', 'temp/');
+     
+//URL Grase Hotspot
+//define('URL_GRASE_QRCODE','http://hpmini/grase/uam/?qrc=');
+    
