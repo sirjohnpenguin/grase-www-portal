4d78fd1f0c6603bef5117a8137b51f1fed64fc99
sirjohnpenguin <sirjohndigweed@gmail.com>
2017-02-23 23:55:15 -0300

    replace userurl with qrcode custom redirect

--- files/usr/share/grase/www/uam/js.php
+++ files/usr/share/grase/www/uam/js.php
@@ -2,12 +2,20 @@
 header("Content-Type: text/javascript; charset=utf-8");
 
 require_once('includes/site.inc.php');
-
+// qrcode
+// set qrcode_user_url
+if ($Settings->getSetting('qrcode')=='TRUE'){
+	$userurl='var userurl=encodeURIComponent("'.$Settings->getSetting('qrcode_user_url').'")';
+}else{
+	$userurl="";
+}
+// qrcode
 $jsfile = basename($_GET['js'], '.js');
 $jsfilecontents = file_get_contents("js/$jsfile.js");
 
 $search = array(
     "###SERVERIPADDRESS###",
+    "###userurl###",
     'Username is required',
     'Are you sure you want to disconnect now?',
     'Error loading generic login form',
@@ -17,6 +25,7 @@ $search = array(
     );
 $replace = array(
     $lanIP,
+    $userurl,
     T_('Username is required'),
     T_('Are you sure you want to disconnect now?'),
     T_('Error loading generic login form'),
